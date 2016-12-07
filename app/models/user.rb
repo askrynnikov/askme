8def7c15ced1e7651ba272ca8ae10dc3240ed5d6
askrynnikov <5101233@gmail.com>
2016-12-07 15:51:37 +0300

    Do the job 49-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
+  validates :email, uniqueness: true
+  validates :username, uniqueness: { case_sensitive: false }
 
   # from Device https://github.com/plataformatec/devise/blob/18b6064d74726147eccd69b24812000074261bbb/lib/generators/templates/devise.rb#L151-L154
   validates :email, format: { with: /\A[^@]+@[^@]+\z/}
@@ -8,4 +9,12 @@ class User < ApplicationRecord
                                  too_long: "%{count} characters is the maximum allowed" }
   validates :username, format: { with: /\A[a-zA-Z0-9_]+\z/,
                                  message: "only letters, numbers, and the sign _" }
+
+  before_save :downcase_username
+
+  private
+
+  def downcase_username
+    username.downcase!
+  end
 end
